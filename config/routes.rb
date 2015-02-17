7bdc8b3bebb2d068788de18328a77c9b02988e97
Brit Butler <redline6561@gmail.com>
2015-02-17 15:08:41 -0500

    Add a way to look up auth token by email and password.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => { :registrations => "registrations",
+                                       :sessions => "sessions" }
 
   get '/games/:id', to: 'games#show'
   # The priority is based upon order of creation: first created -> highest priority.
